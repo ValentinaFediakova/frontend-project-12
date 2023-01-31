b6487bec3499df6b92ea06fc4340bf64322f9a47
Sovch <Filin_999@bk.ru>
2023-01-31 21:47:42 +0400

    tune railways 4

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 
-const path = require('path')
-const AutoLoad = require('@fastify/autoload')
-const fastifyStatic = require('@fastify/static');
-
-module.exports = async function (fastify, opts) {
-  // Place here your custom code!
-
-  fastify.register(fastifyStatic, {
-    root: `${process.cwd()}/my-app/build`,
-  });
-
-  fastify.setNotFoundHandler((req, res) => {
-    res.sendFile('index.html');
-  });
-
-  // Do not touch the following lines
-
-  // This loads all plugins defined in plugins
-  // those should be support plugins that are reused
-  // through your application
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'plugins'),
-    options: Object.assign({}, opts)
-  });
-
-  // This loads all plugins defined in routes
-  // define your routes in one of these
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'routes'),
-    options: Object.assign({}, opts)
-  });
-}
-
 
 function App() {
   return (
